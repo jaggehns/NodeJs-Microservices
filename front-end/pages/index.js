93d104bb8bbf805ac6e992dd067c87a4f020b0e8
Jaggehn <jaggehns@gmail.com>
2023-02-19 12:48:56 +0800

    Implemented skaffold for easy kubernetes deployment

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -5,7 +5,7 @@ import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+function Home({ data }) {
   return (
     <>
       <Head>
@@ -15,8 +15,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Message App</h1>
+        <h1 className={styles.title}>
+          Welcome to Message App - Name: {data.name}, Age: {data.age}
+        </h1>
       </main>
     </>
   );
 }
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://api.agify.io?name=Birdy`);
+  const data = await res.json();
+  console.log(data);
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
+
+export default Home;
